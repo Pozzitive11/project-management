bd74f99ee684591515a2999b52c66bd9c50c8211
Palahin Vladyslav <palahinvlad@gmail.com>
2024-05-07 15:37:38 +0300

    created user roles list and user permissions list

--- src/app/features/user-management/components/user-management-user/user-management-user.component.ts
+++ src/app/features/user-management/components/user-management-user/user-management-user.component.ts
@@ -10,63 +10,25 @@ import { UserManagementUserPermissionsService } from '../../services/user-manage
 import { RoleManagementPermissionService } from 'src/app/features/role-management/services/role-management-permission.service'
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap'
 import { UserManagementRoleComponent } from '../user-management-role/user-management-role.component'
+import { UserManagementPermissionsListComponent } from '../user-management-permissions-list/user-management-permissions-list.component'
+import { UserManagementRolesListComponent } from '../user-management-roles-list/user-management-roles-list.component'
 
 @Component({
   selector: 'app-user-management-user',
   standalone: true,
-  imports: [CommonModule, ModalComponent, NgSelectModule, FormsModule, NgbAccordionModule, UserManagementRoleComponent],
+  imports: [CommonModule, UserManagementPermissionsListComponent, UserManagementRolesListComponent],
   templateUrl: './user-management-user.component.html',
   styleUrl: './user-management-user.component.css'
 })
-export class UserManagementUserComponent implements OnInit, OnChanges {
+export class UserManagementUserComponent {
   protected userManagementUserService = inject(UserManagementUserService)
   protected userManagementUserRolesService = inject(UserManagementUserRolesService)
-  protected userManagementUserPermissionsService = inject(UserManagementUserPermissionsService)
-  protected roleManagementPermissionService = inject(RoleManagementPermissionService)
 
   @Input() user: User | null
-  ngOnInit(): void {
-    this.userManagementUserRolesService.getAvailableRoles()
-    this.roleManagementPermissionService.setApps()
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    this.userManagementUserRolesService.getAvailableRoles()
-    this.userManagementUserPermissionsService.getUserPermissions()
-  }
-  clearDeleteRoleModal() {
-    this.userManagementUserRolesService.selectedRoleForDelete = null
-  }
-  clearAddRoleModal() {
-    this.userManagementUserRolesService.selectedRoleForAdd = null
-  }
-  clearAddPermissionModal() {
-    this.userManagementUserPermissionsService.selectedAppForAdd = null
-    this.userManagementUserPermissionsService.selectedPermissionsForAdd = null
-  }
-  clearDeletePermissionModal() {
-    this.userManagementUserPermissionsService.selectedAppForDelete = null
-    this.userManagementUserPermissionsService.selectedPermissionsForDelete = null
-  }
-  getAvailablePermissions() {
-    if (this.user) {
-      this.userManagementUserPermissionsService.getAvailablePermissionByApp(this.user.id)
-    }
-  }
-  addUserPermission() {
-    if (this.user) {
-      this.userManagementUserPermissionsService.addUserPermission(this.user.id)
-    }
-  }
-  deleteUserPermission() {
-    if (this.user) {
-      this.userManagementUserPermissionsService.deleteUserPermission(this.user.id)
-    }
-  }
-  getSelectedAppForDelete() {
-    if (this.userManagementUserPermissionsService.selectedAppForDelete) {
-      this.userManagementUserPermissionsService.getPermissionsByApp(
-        this.userManagementUserPermissionsService.selectedAppForDelete.app
-      )
-    }
-  }
+  // ngOnInit(): void {
+  //   this.userManagementUserRolesService.getAvailableRoles()
+  // }
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   this.userManagementUserRolesService.getAvailableRoles()
+  // }
 }
